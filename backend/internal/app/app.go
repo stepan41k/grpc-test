00a1f97f097d1c0a28642090fb32e085644bb13a
stepan41k <russia.zont@gmail.com>
2026-04-04 16:44:36 +0300

    refactor: change app structure and app initialization

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -1,21 +1,22 @@
 package app
 
 import (
-	"github.com/go-chi/chi/v5"
-	httpapp "github.com/stepan41k/grpc-test/internal/app/http"
+	"context"
+
+	grpcapp "github.com/stepan41k/grpc-test/internal/app/grpc"
 	"github.com/stepan41k/grpc-test/internal/config"
-	
+	"github.com/stepan41k/grpc-test/internal/storage/postgres"
+
 	"go.uber.org/zap"
 )
 
 type App struct {
-	HTTPServer *httpapp.App
+	gRPCServer *grpcapp.App
 	log        *zap.Logger
 }
 
-func New(log *zap.Logger, cfg *config.Config, router chi.Router) *App {
-	httpApp := httpapp.New(log, cfg, router)
-	app
+func New(ctx context.Context, log *zap.Logger, cfg *config.Config, connectionString string) *App {
+	pool, err := postgres.New(ctx, connectionString)
 
 	return &App{
 		HTTPServer: httpApp,
