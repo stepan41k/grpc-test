2692a57669e9a9e9fb754f6f8089b6e2c23f502c
stepan41k <russia.zont@gmail.com>
2026-04-06 12:37:01 +0300

    refactor: change simple pgxpool to otelpgxpool and add ping to database

--- backend/internal/storage/postgres/postgres.go
+++ backend/internal/storage/postgres/postgres.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/exaring/otelpgx"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -13,12 +14,23 @@ type PGStorage struct {
 
 func New(ctx context.Context, connString string) (*PGStorage, error) {
 	const path = "repository.postgres.New"
-	
-	pool, err := pgxpool.New(ctx, connString)
+
+	connConfig, err := pgxpool.ParseConfig(connString)
 	if err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	connConfig.ConnConfig.Tracer = otelpgx.NewTracer()
+
+	pool, err := pgxpool.NewWithConfig(ctx, connConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pool: %w", err)
+	}
+
+	if err = pool.Ping(ctx); err != nil {
 		return nil, fmt.Errorf("%s: %w", path, err)
 	}
-	
+
 	return &PGStorage{
 		pool: pool,
 	}, nil
