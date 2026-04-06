3a735e0536ff4db7e689a09a7cec7ae03035723f
stepan41k <russia.zont@gmail.com>
2026-04-06 12:42:11 +0300

    refactor: change app initialization and disable stack trace in zap.Logger

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,51 +1,78 @@
 package main
 
 import (
-	"os"
+	"context"
 	"os/signal"
 	"syscall"
-	"fmt"
 
 	"github.com/stepan41k/grpc-test/internal/app"
 	"github.com/stepan41k/grpc-test/internal/config"
+	"github.com/stepan41k/grpc-test/internal/tracing"
 	"go.uber.org/zap"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-    
+
 	log := setupLogger(cfg.Env)
-    
-	log.Info("starting application", zap.Any("config", cfg))
-    
-	storage := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", cfg.PostgreConfig.Host, cfg.PostgreConfig.Port, cfg.PostgreConfig.Username, cfg.PostgreConfig.DBName, os.Getenv("DB_PASSWORD"), cfg.PostgreConfig.SSLMode)
-    
-	application := app.New(log, cfg.GRPCPort, storage)
-    
-    
-	go func() {
-		application.GRPCServer.MustRun()
+
+	log.Info("starting application")
+
+	ctx := context.Background()
+
+	tp, err := tracing.InitTracer(ctx, cfg.OtelConfig.ServiceName, cfg.OtelConfig.URL)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err = tp.Shutdown(ctx)
+		if err != nil {
+			panic("failed to shutdown tracer")
+		}
 	}()
-	
-	//Graceful shutdown
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
-    
-	signal := <-stop
-    
-	log.Info("stopping application", slog.String("signal", signal.String()))
-    
+
+	application := app.New(ctx, log, cfg)
+
+	go func() {
+		if err := application.GRPCServer.Run(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	<-ctx.Done()
+
+	log.Warn("stopping application")
+
+	// simple HTTP graceful shutdown
+	// shutdownCtx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	// defer cancel()
+
 	application.GRPCServer.Stop()
-    
+
 	log.Info("application stopped")
-    }
-    
+}
 
 func setupLogger(env string) *zap.Logger {
 	var log *zap.Logger
 	var err error
 
 	switch env {
+	case envLocal:
+		config := zap.NewDevelopmentConfig()
+		config.DisableStacktrace = true
+		log, err = config.Build()
+		if err != nil {
+			panic("failed to initialize local logger")
+		}
 	case envDev:
 		log, err = zap.NewDevelopment()
 		if err != nil {
