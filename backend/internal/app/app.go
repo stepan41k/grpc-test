42ada23e6e2ac03c4b2a4f02b4c49a431b36e952
stepan41k <russia.zont@gmail.com>
2026-04-06 18:05:05 +0300

    feat: create app.Close method, add storage and tracer to App structure

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -2,37 +2,89 @@ package app
 
 import (
 	"context"
+	"time"
 
 	grpcapp "github.com/stepan41k/grpc-test/internal/app/grpc"
 	"github.com/stepan41k/grpc-test/internal/client"
 	"github.com/stepan41k/grpc-test/internal/config"
 	"github.com/stepan41k/grpc-test/internal/service"
 	"github.com/stepan41k/grpc-test/internal/storage/postgres"
+	"github.com/stepan41k/grpc-test/internal/tracing"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
 	"go.uber.org/zap"
 )
 
 type App struct {
 	GRPCServer *grpcapp.App
+	storage    *postgres.PGStorage
+	tracerProvider *sdktrace.TracerProvider
 	log        *zap.Logger
 }
 
 func New(ctx context.Context, log *zap.Logger, cfg *config.Config) *App {
-	connString2 := config.DTO(cfg)
-	
-	pool, err := postgres.New(ctx, connString2)
+	const path = "app.New"
+
+	log = log.With(
+		zap.String("path", path),
+	)
+
+	tp, err := tracing.InitTracer(ctx, cfg.OtelConfig.ServiceName, cfg.OtelConfig.URL)
 	if err != nil {
-		panic(err)
+		log.Fatal("failed to init tracer", zap.Error(err))
 	}
 
-	client := client.NewGrinexClient("https://grinex.io")
-	
+	connString2 := config.DTO(cfg)
+
+	pool, err := postgres.New(ctx, connString2)
+	if err != nil {
+		log.Fatal("failed to init storage", zap.Error(err))
+	}
+
+	client := client.NewGrinexClient(cfg.GrinexConfig.URL)
+
 	exchangeService := service.New(log, client, pool)
 
 	grpcApp := grpcapp.New(log, exchangeService, cfg.ServerConfig.GRPCPort)
 
 	return &App{
 		GRPCServer: grpcApp,
+		storage:    pool,
+		tracerProvider: tp,
 		log:        log,
 	}
+}
+
+func (a *App) Close(ctx context.Context) {
+	const path = "app.Stop"
+
+	log := a.log.With(
+		zap.String("op", path),
+	)
+
+	log.Warn("starting graceful shutdown")
+
+	a.GRPCServer.Stop()
+	
+	if a.tracerProvider != nil {
+        a.log.Warn("shutting down tracer")
+        
+        shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+        defer cancel()
+
+        if err := a.tracerProvider.Shutdown(shutdownCtx); err != nil {
+            a.log.Error("tracer shutdown failed", zap.Error(err))
+        } else {
+            a.log.Info("tracer stopped successfully")
+        }
+    }
+
+	if a.storage != nil {
+		log.Warn("closing database connection")
+		a.storage.Close(ctx)
+	}
+	
+	log.Info("database connection closing successfully")
+
+	log.Info("graceful shutdown complete")
 }
