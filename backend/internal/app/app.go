0933269a042422d7ef5f767379c65c19f5468e07
stepan41k <russia.zont@gmail.com>
2026-04-06 12:40:55 +0300

    feat: finished linking layers

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -4,22 +4,35 @@ import (
 	"context"
 
 	grpcapp "github.com/stepan41k/grpc-test/internal/app/grpc"
+	"github.com/stepan41k/grpc-test/internal/client"
 	"github.com/stepan41k/grpc-test/internal/config"
+	"github.com/stepan41k/grpc-test/internal/service"
 	"github.com/stepan41k/grpc-test/internal/storage/postgres"
 
 	"go.uber.org/zap"
 )
 
 type App struct {
-	gRPCServer *grpcapp.App
+	GRPCServer *grpcapp.App
 	log        *zap.Logger
 }
 
-func New(ctx context.Context, log *zap.Logger, cfg *config.Config, connectionString string) *App {
-	pool, err := postgres.New(ctx, connectionString)
+func New(ctx context.Context, log *zap.Logger, cfg *config.Config) *App {
+	connString2 := config.DTO(cfg)
+	
+	pool, err := postgres.New(ctx, connString2)
+	if err != nil {
+		panic(err)
+	}
+
+	client := client.NewGrinexClient("https://grinex.io")
+	
+	exchangeService := service.New(log, client, pool)
+
+	grpcApp := grpcapp.New(log, exchangeService, cfg.ServerConfig.GRPCPort)
 
 	return &App{
-		HTTPServer: httpApp,
+		GRPCServer: grpcApp,
 		log:        log,
 	}
 }
