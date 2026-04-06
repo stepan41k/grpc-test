2b9bb3116865017364659bcec388bccb5437c4b2
stepan41k <russia.zont@gmail.com>
2026-04-06 12:40:15 +0300

    feat: add prometheus and otel initialization

--- backend/internal/app/grpc/app.go
+++ backend/internal/app/grpc/app.go
@@ -5,6 +5,8 @@ import (
 	"net"
 
 	"github.com/stepan41k/grpc-test/internal/handler"
+	"github.com/stepan41k/grpc-test/internal/metrics"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
@@ -12,13 +14,20 @@ import (
 type App struct {
 	log        *zap.Logger
 	gRPCServer *grpc.Server
-	port int
+	port       int
 }
 
-func New(log *zap.Logger, exchangeService handler.Exchange, port int) *App {
-	gRPCServer := grpc.NewServer()
-	
-	handler.Register(gRPCServer, exchangeService)
+func New(log *zap.Logger, exchangeService handler.ExchangeService, port int) *App {
+	gRPCServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			metrics.GrpcMetrics.UnaryServerInterceptor(),
+		),
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+	)
+
+	metrics.GrpcMetrics.InitializeMetrics(gRPCServer)
+
+	handler.Register(log, gRPCServer, exchangeService)
 
 	return &App{log: log, gRPCServer: gRPCServer, port: port}
 }
@@ -31,11 +40,19 @@ func (a *App) Run() error {
 		zap.Int("port", a.port),
 	)
 
+	go func() {
+		a.log.Info("starting prometheus metrics server", zap.String("addr", ":9090"))
+
+		if err := metrics.StartMetricsServer(":9090"); err != nil {
+			a.log.Error("prometheus metrics server failed", zap.Error(err))
+		}
+	}()
+
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
 	if err != nil {
 		return fmt.Errorf("%s: %w", path, err)
 	}
-	
+
 	log.Info("starting grpc server", zap.String("addr", l.Addr().String()))
 
 	if err := a.gRPCServer.Serve(l); err != nil {
